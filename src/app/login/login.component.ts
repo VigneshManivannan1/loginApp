ae7781db7092cff349935c89a309c1b1b350f26f
Vignesh Manivannan <vignesh.manivannan@gmail.com>
2022-11-14 12:13:52 +0530

    login app

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  loading = false;
+  submitted = false;
+  error = false;
+
+  constructor(private formBuilder: FormBuilder,
+    public authService:AuthService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+
+  }
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    this.error = false;
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+        return;
+    }
+
+    this.loading = true;
+    this.authService.login(this.f.username.value, this.f.password.value)
+        .subscribe(
+            async data => {
+              localStorage.setItem('token',data);
+              await this.verifyToken(data);
+            },
+            error => {
+                this.error = true;
+                this.loading = false;
+            });
+
+  }
+
+  verifyToken(data: string) {
+    this.authService.verifyToken(data)
+      .subscribe(
+        data => {
+
+          localStorage.setItem('currentUser', data['username']);
+          localStorage.setItem('role', data['role']);
+          this.loading = false;
+          if (data['role'] == "Admin") {
+          }
+          this.authService.isLoggedIn = true;
+          this.router.navigate(['/home']);
+        },
+        error => {
+          this.error = true;
+          this.loading = false;
+        });
+  }
+
+}
